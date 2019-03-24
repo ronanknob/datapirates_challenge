8c70c5b4aa0f5f52da45f8a6e3e20db0963cadaa
Ronan Knob <ronan.knob@gmail.com>
2019-03-24 15:20:06 -0300

    Coletando Localidades e faixas da primeira página com sucesso

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# datapirates_challenge
-Web scraping in Python - Neoway challenge.
+# Data Pirates challenge
+
+Forked from: https://github.com/NeowayLabs/jobs/blob/master/datapirates/challengePirates.md
+
+##Visão geral
+
+
+##Requisitos:
+* Python 3.6;
+* Navegador Chrominum;
+* Pycharm (utilizada versão 2018.3 Community)
+* pip (para instalação de pacotes sugeridos) e pacotes: 
+	* selenium (pip install -U selenium), 
+	* Beautifulsoup (pip install beautifulsoup4), 
+	* requests (pip install requests)
+
+##Como Utilizar
+* Fazer a cópia do projeto a partir do repositório;
+* No Pycharm, carregar o projeto (File -> Open) e navegar até o path;
+* Na classe app.py, clique com o botão direito e selecione a opção Run'app'.
+
+OBS: O navegador Chrominum será aberto automaticamente pelo Webdriver para navegação anônima durante o processo.
+
+
--- app.py
+++ app.py
@@ -42,3 +42,33 @@ seletor_uf.select_by_visible_text(lista_estados[0])
 
 # Clica em Buscar
 driver.find_element_by_xpath("//*[@id='Geral']/div/div/div[4]/input").click()
+
+'''
+Aqui estou coletando os dados da tabela na página resultado. Como há duas tabelas com mesmo nome, 
+uma trazendo o estado resultado, e outra trazendo os resultados em si, escolhi o elemento que contém
+as duas tabelas para fazer o parse.
+'''
+dados = driver.find_element_by_class_name("ctrlcontent")
+html = dados.get_attribute('outerHTML')
+
+# Executa BeautifulSoup para fazer o parser desse HTML
+soupDados = BeautifulSoup(html, "html.parser")
+
+# Obtendo os dois elementos tmptabela presentes na página
+tabelas = soupDados.find_all("table", attrs={'class': 'tmptabela'})
+
+# Selecionando apenas a tabela correta
+tabela_localidades = tabelas[1]
+
+# Guardando nome das localidades numa lista
+localidades = tabela_localidades.find_all('td', attrs={'width': '100'})
+faixas = tabela_localidades.find_all('td', attrs={'width': '80'})
+
+total_linhas = len(faixas) #faixas não possui repetições
+localidades_pos = 0
+faixas_pos = 0
+for x in range(0, total_linhas, 1):
+    print(localidades[localidades_pos].text + " " + faixas[faixas_pos].text)
+    localidades_pos = localidades_pos + 2 #incrementa dois para pular informação de situação
+    faixas_pos = faixas_pos + 1
+
