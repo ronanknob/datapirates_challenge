0ae80a81e7f4ce010d9798600682f666fa76acd1
Ronan Knob <ronan.knob@gmail.com>
2019-03-25 23:33:22 -0300

    Tipo do resultado agora é objeto, arquivo de saída agora como JSON nomeado

---
+++ FaixaCep.py
@@ -0,0 +1,6 @@
+class FaixaCep:
+
+    def __init__(self, estado, localidade, faixa):
+        self.estado = estado
+        self.localidade = localidade
+        self.faixa = faixa
--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 # coding=utf-8
 import time
+
+import stringify as stringify
+from jsonlines.jsonlines import ReaderWriterBase
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 import requests
 import math
 import jsonlines
-
+import FaixaCep
+import json
 
 # Lista final que guardará as tuplas de resultado
 registros_arr = []
 # Inserindo primeira tupla (pode ser usada como cabeçalho no import)
-tupla1 = ("UF", "Localidade", "Faixa de CEP")
-registros_arr.append(tupla1)
 
 # Construindo tabela de UF's com base na disponibilizada no site
 # obtendo dados do endereço
@@ -34,15 +36,14 @@ estados_tam = len(estados)
 for x in range(0, estados_tam, 3):
     lista_estados.append(estados[x:x + 2])
 
-
 # ------ INICIA CRAPING ------------
 for x in range(0, 2, 1):  # Executa com as primeiras 2 UF's da lista
     # inicia o driver do Chrominum
     driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 
-    # acessa o site e aguarda 4s para carregamento da página
+    # acessa o site e aguarda 1s para carregamento da página
     driver.get("http://www.buscacep.correios.com.br/sistemas/buscacep/buscaFaixaCep.cfm")
-    time.sleep(4)
+    time.sleep(1)
 
     # Seleciona o elemento de UF e depois seleciona o estado
     seletor_uf = Select(driver.find_element_by_name("UF"))
@@ -64,9 +65,9 @@ for x in range(0, 2, 1):  # Executa com as primeiras 2 UF's da lista
 
     # Obtem o total de páginas de resultado para checar se há mais de uma.
     qt_paginas = soupDados.text
-    qt_paginas = int(qt_paginas[qt_paginas.find("de")+3:qt_paginas.find("de")+7])
+    qt_paginas = int(qt_paginas[qt_paginas.find("de") + 3:qt_paginas.find("de") + 7])
     if qt_paginas >= 50:
-        qt_paginas = math.ceil(qt_paginas/50)  # valor inteiro antecessor mais próximo
+        qt_paginas = math.ceil(qt_paginas / 50)  # valor inteiro antecessor mais próximo
     else:
         qt_paginas = 1
 
@@ -81,15 +82,16 @@ for x in range(0, 2, 1):  # Executa com as primeiras 2 UF's da lista
         localidades = tabela_localidades.find_all('td', attrs={'width': '100'})
         faixas = tabela_localidades.find_all('td', attrs={'width': '80'})
 
-        total_linhas = len(faixas) # faixas não possui repetições
+        total_linhas = len(faixas)  # faixas não possui repetições
         localidades_pos = 0
         faixas_pos = 0
         for z in range(0, total_linhas, 1):
             trim_faixas = str(faixas[faixas_pos].text)
             trim_faixas = trim_faixas.strip(" ")
-            tupla = (lista_estados[x], localidades[localidades_pos].text, trim_faixas)
-            registros_arr.append(tupla)  # Incremento na lista global
-            localidades_pos = localidades_pos + 2 # incrementa dois para pular informação de situação
+            faixacep = FaixaCep.FaixaCep(lista_estados[x], localidades[localidades_pos].text, trim_faixas)
+            # tupla = (lista_estados[x], localidades[localidades_pos].text, trim_faixas)
+            registros_arr.append(faixacep)  # Incremento na lista global
+            localidades_pos = localidades_pos + 2  # incrementa dois para pular informação de situação
             faixas_pos = faixas_pos + 1
     else:
         for y in range(0, qt_paginas, 1):
@@ -97,7 +99,7 @@ for x in range(0, 2, 1):  # Executa com as primeiras 2 UF's da lista
             tabelas = soupDados.find_all("table", attrs={'class': 'tmptabela'})
 
             # Selecionando apenas a tabela correta
-            tabela_localidades = tabelas[len(tabelas)-1]
+            tabela_localidades = tabelas[len(tabelas) - 1]
 
             # Guardando nome das localidades numa lista
             localidades = tabela_localidades.find_all('td', attrs={'width': '100'})
@@ -109,17 +111,19 @@ for x in range(0, 2, 1):  # Executa com as primeiras 2 UF's da lista
             for z in range(0, total_linhas, 1):
                 trim_faixas = str(faixas[faixas_pos].text)
                 trim_faixas = trim_faixas.strip(" ")
-                tupla = (lista_estados[x], localidades[localidades_pos].text, trim_faixas)
-                registros_arr.append(tupla)  # Incremento na lista global
+                trim_faixas = trim_faixas.strip(" ")
+                faixacep = FaixaCep.FaixaCep(lista_estados[x], localidades[localidades_pos].text, trim_faixas)
+                # tupla = (lista_estados[x], localidades[localidades_pos].text, trim_faixas)
+                registros_arr.append(faixacep)  # Incremento na lista global
                 localidades_pos = localidades_pos + 2  # incrementa dois para pular informação de situação
                 faixas_pos = faixas_pos + 1
 
-            if y <= qt_paginas-2:  # Só executa o click em próxima página até a penúltima página.
+            if y <= qt_paginas - 2:  # Só executa o click em próxima página até a penúltima página.
                 # Vai para próxima paǵina
                 url_proxima = "javascript:document.Proxima.submit('Proxima')"
                 driver.find_element_by_xpath('//a[@href="' + url_proxima + '"]').click()
                 # Espera para carregar os dados da página
-                time.sleep(3)
+                time.sleep(2)
                 # Atualiza dados das variáveis
                 dados = driver.find_element_by_class_name("ctrlcontent")
                 html = dados.get_attribute('outerHTML')
@@ -130,10 +134,6 @@ for x in range(0, 2, 1):  # Executa com as primeiras 2 UF's da lista
 
 # Fazer o export da lista para um JSONL
 with jsonlines.open('./output.jsonl', mode='w') as writer:
-    for x in range(0, len(registros_arr), 1):
-        writer.write(registros_arr[x])
+    for i in range(0, len(registros_arr), 1):
+        writer.write(vars(registros_arr[i]))
 writer.close()
-
-
-
-
